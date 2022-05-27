f8f934b27143e20cc9217c2d01f76bb5440f81f5
emmaofori20 <emmanuelofori2638@gmail.com>
2022-05-27 15:58:20 +0000

    first commit

---
+++ Amoponsah/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Amoponsah.Models;
+using Amoponsah.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amoponsah.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly MailService mailService;
+
+        public HomeController(ILogger<HomeController> logger, MailService mailService)
+        {
+            _logger = logger;
+            this.mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        } 
+        
+        [HttpPost]
+        public IActionResult Index(MailViewModel model)
+        {
+            mailService.sendMail(model);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Amoponsah/MailService.cs
@@ -0,0 +1,45 @@
+﻿using Amoponsah.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Amoponsah
+{
+    public class MailService
+    {
+        public MailService()
+        {
+
+        }
+
+        public void sendMail( MailViewModel model)
+        {
+
+            MailAddress to = new MailAddress("emmanuelofori2638@gmail.com");
+            MailAddress from = new MailAddress(model.Email);
+
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = model.Subject;
+            message.Body = model.Message;
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("emmanuelofori2638@gmail.com", "emmaofori20"),
+                EnableSsl = true
+            };
+
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception err)
+            {
+                var error =err.Message;
+                
+            }
+        }
+    }
+}
